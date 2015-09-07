6e5bd17ce44a3f64759415b87942ed639d9711a1
Rick <rick.van.rheenen@gmail.com>
2015-09-07 08:09:23 +0200

    ADT IndentifierInterface

--- src/assignment1/IdentifierInterface.java
+++ src/assignment1/IdentifierInterface.java
@@ -1,5 +1,30 @@
 package assignment1;
 
+/** ADT for the class Identifier.
+*
+* @author Pim Dijt, Rick van Rheenen
+* 
+* @elements
+*  Characters of type char.
+* @structure
+*	Lineair
+* @domain
+*	All series of alfanumeric characters starting with a letter and containing at least one letter.
+* @constructor
+*	Identifier();
+*	    <dl>
+*		<dt><b>PRE-conditie</b><dd>-
+*		<dt><b>POST-conditie</b><dd>
+*	    </dl>
+*	<br>
+*	Identifier (Identifier src);
+*	    <dl>
+*		<dt><b>PRE-conditie</b><dd>-
+*		<dt><b>POST-conditie</b><dd>
+*	    </dl>
+**/
+
+
 public interface IdentifierInterface {
 	
 	
