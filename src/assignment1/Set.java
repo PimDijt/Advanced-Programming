3d73cdfe2516e189af39f38f200f41db41f38668
Rick <rick.van.rheenen@gmail.com>
2015-09-09 17:03:23 +0200

    commit

--- src/assignment1/Set.java
+++ src/assignment1/Set.java
@@ -26,7 +26,12 @@ public class Set implements SetInterface {
 		return size;
 	}
 
+	public boolean contains(Identifier id) {
+		return false;
+	}
+	
 	public Identifier getIdentifier() {
+		
 		return null;
 	}
 	
@@ -40,39 +45,60 @@ public class Set implements SetInterface {
 		size++;
 	}
 
-
 	public void removeIdentifier(Identifier id) {
 		int toRemove = 0;
 		
 		for (int i = 0; i < size; i++){ // Get index of the item that has to be removed
-				if (col[i] == id){
+				if (elements[i] == id){
 					toRemove = i;
 					break;
 				}
 			}
 		
 		for (int i = toRemove; i < size; i++){ // "Remove" items by overwriting by following items
-			col[i] = col[i+1];
+			elements[i] = elements[i+1];
 		}
 		size--;
 	}
 
-	public Set difference(Set set) {
+	public Set difference(Set src) {
+		Set result = new Set(this);
+		Set foreignSet = new Set(src);
+		while (foreignSet.getSize() > 0) {
+			Identifier tmp = foreignSet.getIdentifier();
+			result.removeIdentifier(tmp);
+			foreignSet.removeIdentifier(tmp);
+		}
+		
+		return result;
+	}
+
+	public Set union(Set src) {
+		Set result = new Set(this);
+		Set foreignSet = new Set(src);
+		while (foreignSet.getSize() > 0) {
+			Identifier tmp = foreignSet.getIdentifier();
+			foreignSet.removeIdentifier(tmp);
+			if (!result.contains(tmp)){
+				try {
+					result.addIdentifier(tmp);
+				} catch (Exception e) {
+					e.printStackTrace(System.out);
+				}
+			}
+		}
+		return result;
+	}
+
+	public Set intersection(Set src) {
 		return null;
 	}
 
-	public Set union(Set set){
+
+	public Set symmetricDifference(Set src) {
 		return null;
 	}
 
-	public Set intersection(Set set) {
-		return null;
-	}
-
-
-	public Set symmetricDifference(Set set) {
-		return null;
-	}
 
 
 }
