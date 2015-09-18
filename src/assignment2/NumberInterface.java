851a8a7330f99a2f1da15732c98ad47aba892b6b
Rick <rick.van.rheenen@gmail.com>
2015-09-18 23:39:53 +0200

    minor

--- src/assignment2/NumberInterface.java
+++ src/assignment2/NumberInterface.java
@@ -1,5 +1,31 @@
 package assignment2;
 
+/**
+ * ADT for the class Number.
+ *
+ * @author Pim Dijt, Rick van Rheenen
+ * 
+ * @elements A natural number
+ * @structure none
+ * @domain A collection of between 0 and 20 Identifiers 
+ * @constructor Collection();
+ *              <dl>
+ *              <dt><b>PRE-condition</b>
+ *              <dd>-
+ *              <dt><b>POST-condition</b>
+ *              <dd>The new Collection object contains 0 Identifier objects
+ *              </dl>
+ *              <br>
+ *              Collection (Collection src);
+ *              <dl>
+ *              <dt><b>PRE-condition</b>
+ *              <dd>-
+ *              <dt><b>POST-condition</b>
+ *              <dd>The new Collection object is a copy of src
+ *              </dl>
+ *
+ **/
+
 public interface NumberInterface {
 
 }
