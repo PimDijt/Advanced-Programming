f1a2e1cba78283909023c5b178df09f76fc50471
rick <rick@DESKTOP-CKOOJQV>
2015-09-24 13:02:27 +0200

    ListInterface Styling

--- src/assignment2/ListInterface.java
+++ src/assignment2/ListInterface.java
@@ -1,23 +1,27 @@
 package assignment2;
 
-/**	@elements : objects of type E
- *	@structure : lineair
- *	@domain : 	The elements in the list are sorted monotonically increasing. 
- *				All rows of elements of type E are valid values for a list.
- *       		For every non-empty list the reference current is pointing to an 
- *				element in the list.
- *	@constructor - List();
- *	<dl>
- *		<dt><b>PRE-conditie</b><dd>		-
- *		<dt><b>POST-conditie</b><dd> 	The new List-object is the empty list.
- * </dl>
+/**
+ * @elements : objects of type E
+ * @structure : lineair
+ * @domain : The elements in the list are sorted monotonically increasing. All
+ *         rows of elements of type E are valid values for a list. For every
+ *         non-empty list the reference current is pointing to an element in the
+ *         list.
+ * @constructor - List();
+ *              <dl>
+ *              <dt><b>PRE-conditie</b>
+ *              <dd>-
+ *              <dt><b>POST-conditie</b>
+ *              <dd>The new List-object is the empty list.
+ *              </dl>
  **/
 
 public interface ListInterface<E extends Data<E>> extends Clonable<ListInterface<E>> {
 
-	/**	@precondition -
-	 *  @postcondition - FALSE: list is not empty.
-	 *  				TRUE:  list is empty.
+	/**
+	 * @precondition  -
+	 * @postcondition - TRUE: list is empty.
+	 * 					FALSE: list is not empty.
 	 **/
 	boolean isEmpty();
 
@@ -26,15 +30,16 @@ public interface ListInterface<E extends Data<E>> extends Clonable<ListInterface
 	 **/
 	ListInterface<E> init();
 
-	/**	@precondition  -
-	 *	@postcondition - The number of elements has been returned.
+	/**
+	 * @precondition -
+	 * @postcondition - The number of elements has been returned.
 	 **/
 	int size();
 
-	/** @precondition  -
-	 *	@postcondition - A copy of d has been added to List-PRE.
-	 *    				current points to the newly added element.
-	 *   				list-POST has been returned.
+	/**
+	 * @precondition -
+	 * @postcondition - A copy of d has been added to List-PRE. current points
+	 *                to the newly added element. list-POST has been returned.
 	 **/
 	ListInterface<E> insert(E d);
 
@@ -45,8 +50,9 @@ public interface ListInterface<E extends Data<E>> extends Clonable<ListInterface
 	E retrieve();
 
 
-	/** @precondition  - The list is not empty.
-	 * 	@postcondition - The current element of list-PRE is not present in list-POST.
+	/** 
+	 * @precondition  - The list is not empty.
+	 * @postcondition - The current element of list-PRE is not present in list-POST.
 	 * 	    			current-POST points to
 	 *    					- if list-POST is empty
 	 *   						null
@@ -55,13 +61,14 @@ public interface ListInterface<E extends Data<E>> extends Clonable<ListInterface
 	 *     							the last element of list-POST
 	 *     						else 
 	 *     							the element after current-PRE 
-	 *  				list-POST has been returned.
+	 *  							list-POST has been returned.
 	 **/
 	ListInterface<E> remove();
 
 
-	/** @precondition  - 
-	 *	@postcondition - TRUE:  list contains a copy of d.
+	/** 
+	 * @precondition  - 
+	 * @postcondition - TRUE:  list contains a copy of d.
 	 *	     			current-POST points to the first element in list that
 	 *	     			contains a copy of d.
 	 *     				FALSE: list does not contain a copy of d.
@@ -76,35 +83,40 @@ public interface ListInterface<E extends Data<E>> extends Clonable<ListInterface
 	boolean find(E d);
 
 
-	/** @precondition  - 
-	 *	@postcondition - FALSE: list is empty
+	/** 
+	 * @precondition  - 
+	 * @postcondition - FALSE: list is empty
 	 *    				TRUE:  current points to the first element
 	 **/
 	boolean goToFirst();
 
 
-	/**	@precondition  - 
-	 *	@postcondition - FALSE: list is empty
-	 *     				TRUE:  current points to the last element
+	/**	
+	 * @precondition - 
+	 *@postcondition - FALSE: list is empty
+	 *     			   TRUE:  current points to the last element
 	 */
 	boolean goToLast();
 
 
-	/** @precondition  - 
-	 *	@postcondition - FALSE: list is empty or current points to the last element
+	/** 
+	 * @precondition  - 
+	 * @postcondition - FALSE: list is empty or current points to the last element
 	 *     				TRUE:  current-POST points to the next element of current-PRE
 	 */
 	boolean goToNext();
 
 
-	/** @precondition  - 
-	 *	@postcondition - FALSE: list is empty of current wijst het eerste object aan
+	/**
+	 * @precondition  - 
+	 * @postcondition - FALSE: list is empty of current wijst het eerste object aan
 	 *     				TRUE:  current-POST points to the prior element of current-PRE
 	 */
 	boolean goToPrevious();
 
-	/** @precondition  -
-	 *	@postcondition - A deep-copy of list has been returned.
+	/** 
+	 * @precondition  -
+	 * @postcondition - A deep-copy of list has been returned.
 	 **/
 	public ListInterface<E> clone();
 }
