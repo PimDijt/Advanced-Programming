0b8e1eb7ac9567686e3268de2d8bc80a44779b43
PimDijt <pimdijt@gmail.com>
2015-10-04 15:01:55 +0200

    Eerste opzet Main, kan simple assignment opslaan en printen

--- src/assignment2/Main.java
+++ src/assignment2/Main.java
@@ -1,34 +1,41 @@
 package assignment2;
 
+import java.io.PrintStream;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
 	
 	Scanner in;
+	PrintStream out;
+	Table<Identifier, Set<Number>> table;
 
 	Main() {
 		in = new Scanner(System.in);
+		out = new PrintStream(System.out);
+		table = new Table<Identifier,Set<Number>>();
 	}
 	
 	void start(){
-		String 	row = in.nextLine();
+		while(in.hasNextLine()){
+			try {
+				program(in);
+			} catch (APException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	void program(Scanner input) throws APException {
+		String row = input.nextLine();
+		row = row.replaceAll("\\s+", "");
 		Scanner rowScanner = new Scanner(row);
-		try {
-			program(rowScanner);
-		} catch (APException e) {
-			e.printStackTrace();
-		}
+		rowScanner.useDelimiter("");
+		statement(rowScanner);
+
 	}
 	
-	void program(Scanner input) throws APException{
-		while(!eof(input)){
-			Scanner rowScanner = new Scanner(in.nextLine()).useDelimiter("");
-			statement(rowScanner);				
-		}
-	}
-	
-	void statement(Scanner input) throws APException{
+	void statement(Scanner input) throws APException{		
 		if(nextCharIsLetter(input)){
 			assignment(input);
 		}else if(nextCharIs(input, '?')){
@@ -43,14 +50,32 @@ public class Main {
 	void assignment(Scanner input) throws APException{
 		Identifier id = identifier(input);
 		character(input, '=');
-		Set set = expression(input);
+		Set<Number> set = expression(input);
 		eoln(input);
+		table.addKeyValue(id, set);
 	}
 	
-	void printStatement(Scanner input){
+	void printStatement(Scanner input) throws APException{
 		character(input, '?');
-		Set set = expression(input);
+		Set<Number> set = expression(input);
 		eoln(input);
+		printSet(set);
+	}
+	
+	void printSet(Set<Number> set){
+		Set<Number> clone = set.clone();
+		while(!clone.isEmpty()){
+			Number number = clone.getElement();
+			clone.removeElement(number);
+			printNumber(number);
+		}
+		out.printf("\n");
+	}
+	
+	void printNumber(Number number){
+		for(int i=0; i<number.size(); i++){
+			out.printf("%c ", number.getChar(i));
+		}
 	}
 	
 	void comment(Scanner input) throws APException{
@@ -62,21 +87,116 @@ public class Main {
 	}
 	
 	
-	Identifier identifier(Scanner input){
-		Identifier id = new Identifier().init(nextChar(input));
-		while(1==1){
-			//
+	Identifier identifier(Scanner input) throws APException{
+		Identifier id = new Identifier();
+		if(!nextCharIsLetter(input)){
+			throw new APException("Identifier has to start with a letter!");
+		}
+		id.init(nextChar(input));
+		
+		while(nextCharIsLetter(input) || nextCharIsDigit(input)){
+			id.addChar(nextChar(input));
 		}
 		return id;
 	}
 
-	Set<E> expression(Scanner input){
-		return null;
+	Set<Number> expression(Scanner input) throws APException{
+		Set<Number> set = term(input);
+		while(nextCharIsAdditiveOperator(input)){
+			if(nextCharIs(input, '+')){
+				character(input, '+');
+				
+				//union
+			}else if(nextCharIs(input, '-')){
+				character(input, '-');
+				//difference
+			}else if(nextCharIs(input, '|')){
+				character(input, '|');
+				//symdiff
+			}
+		}
+		return set;
+	}
+	
+	Set<Number> term(Scanner input) throws APException{
+		Set<Number> set = factor(input);
+		while(nextCharIs(input, '*')){
+			character(input, '*');
+			// intersection
+		}
+		return set;
+	}
+	
+	Set<Number> factor(Scanner input) throws APException{
+		Set<Number> set = new Set<Number>();
+		if(nextCharIsLetter(input)){
+			Identifier id = identifier(input);
+			set = table.getValue(id);
+		} else if(nextCharIs(input, '(')){
+			set = complexFactor(input);
+		} else{
+			set = set(input);
+		}
+		return set;
+	}
+	
+	Set<Number> complexFactor(Scanner input) throws APException{
+		character(input, '(');
+		Set<Number> set = expression(input);
+		character(input, ')');
+		return set;
+	}
+	
+	Set<Number> set(Scanner input) throws APException{
+		character(input, '{');
+		Set<Number> set = rowNaturalNumbers(input);
+		character(input, '}');
+		return set;
+	}
+	
+	Set<Number> rowNaturalNumbers(Scanner input) throws APException{
+		Set<Number> set = new Set<Number>();
+		if(nextCharIs(input, '}')){
+			//empty set
+			return set;
+		}
+		Number first = naturalNumber(input);
+		set.addElement(first);
+		while(nextCharIs(input, ',')){
+			character(input, ',');
+			Number number = naturalNumber(input);
+			set.addElement(number);
+		}
+		return set;
+	}
+ 	
+	Number naturalNumber(Scanner input) throws APException{
+		Number num = new Number();
+		if(!nextCharIsDigit(input)){
+			throw new APException("Numbers have to start with a digit");
+		}
+		num.init(nextChar(input));
+		
+		if(num.getChar(0) == '0'){
+			if(!nextCharIsDigit(input)){
+				return num;
+			}
+			throw new APException("Numbers larget han 0 cannot start with a 0");
+		}
+		
+		while(nextCharIsDigit(input)){
+			num.addDigit(nextChar(input));
+		}
+		return num;		
+	}
+	
+	boolean nextCharIsAdditiveOperator(Scanner input){
+		return nextCharIs(input, '+') || nextCharIs(input, '|') || nextCharIs(input, '-');
 	}
 	
 	void character (Scanner input, char c) throws APException {
 	    if (! nextCharIs(input, c)) {
-	    	throw new APException("........");
+	    	throw new APException("Read " + nextChar(input) + " and expected " + c + "");
 	    }
 	    nextChar(input);
 	}
@@ -90,12 +210,9 @@ public class Main {
 	    	throw new APException("........");
 	    }
 	}
-	
-	boolean eof (Scanner input) throws APException {
-	    if (input.hasNext()) {
-	    	return true;
-	    }
-	    return false;
+		
+	boolean nextCharIsDigit (Scanner in) {
+		return in.hasNext("[0-9]");
 	}
 	
 	boolean nextCharIs(Scanner in, char c) {
